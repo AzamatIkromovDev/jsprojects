7d827da08f37c7c1e616736b371217ff5d8a2b9a
AzamatIkromovDev <AzamatIkromovDev@gmail.com>
2023-12-11 15:46:36 +0500

    answers

--- 01-Fundamentals-Part-1/starter/main.js
+++ 01-Fundamentals-Part-1/starter/main.js
@@ -886,23 +886,23 @@
 //
 // instructor.hasOwnProperty();
 
-const charCounts = (str) => {
-  // we shoukd create new empty object
-  let result = {};
-  //WE SHOULD LOOP EACH CHARACTER
-  for (let i = 0; i < str.length; i++) {
-    let char = str[i].toLowerCase();
-    if (result[char] > 0) {
-      result[char]++;
-    } else {
-      result[char] = 1;
-    }
-  }
-  return result;
-  //WE SHOULD count eaach charachter if it is grater than 1 count ++
-  //We should return result
-};
-
-console.log(charCounts("Man Azamat Ikromov"));
-
-/[a-z0-9]/.test(char);
+// const charCounts = (str) => {
+//   // we shoukd create new empty object
+//   let result = {};
+//   //WE SHOULD LOOP EACH CHARACTER
+//   for (let i = 0; i < str.length; i++) {
+//     let char = str[i].toLowerCase();
+//     if (result[char] > 0) {
+//       result[char]++;
+//     } else {
+//       result[char] = 1;
+//     }
+//   }
+//   return result;
+//   //WE SHOULD count eaach charachter if it is grater than 1 count ++
+//   //We should return result
+// };
+//
+// console.log(charCounts("Azamat Ikromov"));
+//
+// /[a-z0-9]/.test(char);
